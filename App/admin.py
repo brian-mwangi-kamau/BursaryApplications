ef3b12512376b4a7cf5db0f88c3f2fd976ab6839
Brian Mwangi <brianmwangikamau4@gmail.com>
2023-08-21 09:15:18 +0300

    Restart

--- App/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import User
-from .models import BursaryApplication
-
-
-
-admin.site.register(User)
-admin.site.register(BursaryApplication)
-#admin.site.register(DatabaseComparison)
