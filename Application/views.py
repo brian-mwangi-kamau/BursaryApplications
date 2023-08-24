f45b9e88e799f939d8be961426d9182faaa39091
Brian Mwangi <brianmwangikamau4@gmail.com>
2023-08-24 16:33:17 +0300

    ....

--- Application/views.py
+++ Application/views.py
@@ -1,20 +1,27 @@
-import os
-from django.shortcuts import render, redirect
-#from .models import SiteSettings
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
+from django.shortcuts import render
 from .forms import ApplicationForm
 from .models import Application
-from .models import VoterDatabase
- 
-# Imports for email functionality
 import smtplib
-from django.core.mail import send_mail
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
-from email.utils import formataddr
+from django.db import connections
+import os
 from dotenv import load_dotenv
 load_dotenv()
 
-# Create your views here.
+
+def get_voter_info(id_number):
+    with connections['external_database'].cursor() as cursor:
+        cursor.execute("SELECT constituency, location FROM voter WHERE id_number = %s", [id_number])
+        row = cursor.fetchone()
+        if row:
+            return row[0], row[1]
+        return None, None
+    
+
+
 def application_form(request):
     if request.method == 'POST':
         form = ApplicationForm(request.POST)
@@ -31,66 +38,71 @@ def application_form(request):
                 id_number=form_data['id_number']
             )
             application.save()
+
+            voter_constituency, voter_location = get_voter_info(form_data['id_number'])
+
+            if (voter_constituency == form_data['constituency'] and
+                voter_location == form_data['location']):
+                save_to_googlesheets(form_data)
+            else:
+                send_email(form_data)
+
             return render(request, 'success.html')
-    else:
-        form = ApplicationForm()
-
-    return render(request, 'Application.html', {'form': form})
-
-
-def success(request):
-    return render(request, 'success.html')
-
-
-
-# View for Comparison
-def process_applications():
-    external_data = VoterDatabase.objects.using('external_database').all()
-
-    to_spreadsheets = []
-    manual_review = []
-
-    for app in Application.objects.all():
-        if app.id_number in external_data and \
-           app.constituency == external_data[app.id_number]['constituency'] and \
-           app.location == external_data[app.id_number]['location']:
-            to_spreadsheets.append(app) # This will happen with the logic for sheets working
         else:
-            manual_review.append(app) # Sending to the admin's email provided below
-
-    send_to_mail(manual_review)
-
-# views for mails
-# Email configuration
-smtp_server = os.environ['smtp_server']
-smtp_port = os.environ['smtp_port']
-sender_email = os.environ['sender_email']
-sender_password = os.environ['sender_password']
-recipient_email = os.environ['recipient_email']
+            form = ApplicationForm()
+        return render(request, 'Application.html', {'form': form})
 
 
-def send_to_mail(applications):
-    for app in applications:
-        subject = f"Application needs Manual Review - {app.student_name}"
-        message = f"Application details:\nStudent Name: {app.student_name}\nSchool Name: {app.school_name}\nAdmission Number: {app.admission_number}\nForm/Year:{app.year_of_study}\nLocation: {app.location}\nPhone Number: {app.phone_number}\nID Number: {app.id_number}"
-        
-        msg = MIMEMultipart()
-        msg['From'] = formataddr(('Bursary system', sender_email))
-        msg['To'] = recipient_email
-        msg['Subject'] = subject
+def save_to_googlesheets(data):
+    scope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/spreadsheets",
+             "https://www.googleapis.com/auth/drive.file", "https://www.googleapis.com/auth/drive"]
+    
+    credentials = ServiceAccountCredentials.from_json_keyfile_name('google/credentials.json', scope)
+    client = gspread.authorize(credentials)
 
-        msg.attach(MIMEText(message, 'plain'))
+    spreadsheet = client.open('BursaryApplications')
 
-        try:
-            server = smtplib.SMTP(smtp_server, smtp_port)
-            server.starttls()
-            server.login(sender_email, sender_password)
-            server.sendmail(sender_email, recipient_email, msg.as_string)
-            server.quit()
-            print('Email sent successfully!')
-        except Exception as e:
-            print('Error:', e)
+    worksheet = spreadsheet.get_worksheet(0)
 
-    if __name__ == '__main__':
-        process_applications()
+    worksheet.append_row([
+        data['student_name'],
+        data['school_name'],
+        data['admission_number'],
+        data['year_of_study'],
+        data['location']
+    ])
 
+
+
+def send_email(form_data):
+    subject = 'New Application Review'
+    message = f"Application details:\n\n" \
+              f"Name: {form_data['student_name']}\n" \
+              f"School: {form_data['school_name']}\n" \
+              f"Admission Number: {form_data['admission_number']}\n" \
+              f"Year of Study: {form_data['year_of_study']}\n" \
+              f"Constituency: {form_data['constituency']}\n" \
+              f"Location: {form_data['location']}\n" \
+              f"Phone Number: {form_data['phone_number']}\n" \
+              f"ID Number: {form_data['id_number']}\n"
+    
+    smtp_server = 'smtp.gmail.com'
+    smtp_port = 587
+    smtp_username = os.environ('EMAIL_HOST_USER')
+    smtp_password = os.environ('EMAIL_HOST_PASSWORD')
+    recipient_email = os.environ('RECIPIENT_EMAIL')
+
+    from_email = smtp_username
+    to_email = recipient_email
+
+    msg = MIMEMultipart()
+    msg['From'] = from_email
+    msg['To'] = to_email
+    msg['Subject'] = subject
+    msg.attach(MIMEText(message, 'plain'))
+
+
+    with smtplib.SMTP(smtp_server, smtp_port) as server:
+        server.starttls()
+        server.login(smtp_username, smtp_password)
+        server.sendmail(from_email, to_email, msg.as_string())
--- Project-applications/urls.py
+++ Project-applications/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for applications project.
+URL configuration for Bursaries project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.2/topics/http/urls/
@@ -20,5 +20,4 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('Application.urls')),
-    #path('', include('GoogleSpreadsheets.urls')),
 ]
